4f497f4cd2dddc2f631ef58a7dd532f244c95431
adnerdable <adnerdable@gmail.com>
2024-05-24 21:11:32 -0400

    Add file structure

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Button that increments by one each time that it is clicked
+//Button that decreases (for mistakes)
+//Button that says total tally? 
+//If total =>60 then the screen will say "congratulations you contributed to your overlords happiness"
+//If total =<60 then the screen says "There are green pastures for you" and a cow shows on the screen
